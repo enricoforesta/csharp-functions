6a9ebc3b1ec54ffd7c4aa78ff1bb6ce48dc077ae
enricoforesta <enrico.foresta97@gmail.com>
2024-04-18 15:02:07 +0200

    feat: fine esercizio

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -4,7 +4,69 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Funzioni");
+            // Stampa array
+            void StampaVirgolaESpazio()
+            {
+                Console.Write(", ");
+            }
+            void StampaArray(int[] array)
+            {
+                Console.Write("[");
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i]);
+                    if (i < array.Length - 1)
+                        StampaVirgolaESpazio();
+                }
+                Console.Write("]");
+                Console.WriteLine("");
+            }
+
+            // Stampa numero al quadrato 
+            int Quadrato(int numero)
+            {
+                int result = numero * numero;
+                return result;
+            }
+
+            // Stampa Array al quadrato
+            int[] ElevaArrayAlQuadrato(int[] array)
+            {
+                int[] resultArray = (int[])array.Clone();
+                for (int i = 0;i < resultArray.Length; i++)
+                {
+                    resultArray[i] = Quadrato(resultArray[i]);
+                }
+                return resultArray;
+            }
+            // Stampa Somma Array
+            int SommaElementiArray(int[] array)
+            {
+                int somma = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    somma += array[i];
+                }
+                return somma;
+            }
+
+            // Stampa array
+            int[] arrayNum = [2, 6, 7, 5, 3, 9];
+            Console.WriteLine($"Array originale");
+            StampaArray(arrayNum);
+
+
+            // Stampa Array al quadrato
+            Console.WriteLine("Array copia");
+            StampaArray((ElevaArrayAlQuadrato(arrayNum)));
+            Console.WriteLine("Array originale");
+            StampaArray(arrayNum);
+
+            // Stampa Somma numeri Array
+            Console.WriteLine($"La somma dei numeri è {SommaElementiArray(arrayNum)}");
+
+            //Stampa Somma numeri al quadrato Array
+            Console.WriteLine($"La somma dei numeri è { SommaElementiArray(ElevaArrayAlQuadrato(arrayNum))}");
         }
     }
 }
